8a99363362d71ac29ccc3437416163db4c750db2
eugeniarossi <eugenia.rossi@outlook.com>
2023-02-23 18:50:15 +0100

    Milestone 3 finito

--- js/main.js
+++ js/main.js
@@ -11,17 +11,25 @@ for (let i = 1; i <= 100; i++) {
     // Definisco la variabile divElement in cui creo un elemento div
     const divElement = document.createElement('div');
 
+    // Assegno all'elemento divElement la classe div-element
+    divElement.classList.add('div-element');
+
+    // Definisco delle condizioni
     if ((i % 3 === 0) && (i % 5 === 0)) {
         x = 'FizzBuzz';
+        divElement.classList.add('fizzbuzz');
 
     } else if (i % 5 === 0) {
         x = 'Buzz';
+        divElement.classList.add('buzz');
 
     } else if (i % 3 === 0) {
         x = 'Fizz';
+        divElement.classList.add('fizz');
 
     } else {
         x = i;
+        divElement.classList.add('number');
     }
 
     // Stampo la variabile x in console
@@ -29,7 +37,7 @@ for (let i = 1; i <= 100; i++) {
 
     // Aggiungo alla variabile divElement la variabile x (stringa da mostrare)
     divElement.append(x);
-    
+
     // Aggiungo divElement al container 
     container.append(divElement);
 }
